0f380a8f564d16dc40db969829a0195ae6621685
Ben Thomas <theconnectome@users.noreply.github.com>
2015-01-26 15:50:37 +0200

    Update cachematrix.R
    Added some comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,19 @@
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
+        
+        # set the value of the matrix
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+        
+        # get the value of the matrix
         get <- function() x
+        
+        # set the value of the matrix's inverse
         setinverse <- function(solve) m <<- solve
+        
+        # get the value of the matrix's inverse
         getinverse <- function() m
         list(set = set, get = get,
              setinverse = setinverse,
@@ -13,13 +21,21 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 cacheSolve <- function(x, ...) {
+
+        # get the value of the matrix from the previous function
         m <- x$getinverse()
+        
+        # if that value is not null, they fetch it from the cache
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
         data <- x$get()
+        
+        # otherwise, calculate the inverse of the matrix...
         m <- solve(data, ...)
+        
+        # ...and set the value of the new matrix!
         x$setinverse(m)
         m
 }
